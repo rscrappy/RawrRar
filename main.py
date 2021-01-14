a12a6eff7d0094941885251eeb37e7f24d0b5206
Furrure <71794171+Furrure@users.noreply.github.com>
2021-01-13 21:38:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ pages['home'] = opened.read()
 opened.close()
 HTTP_server = None
 def response_handler(client_obj):
-	print(client_obj.split_request)
+
 	if client_obj.split_request == []:
 		client_obj.set_page(pages['home'])
 	elif client_oj.split_request[0] == "REINIT":
@@ -21,6 +21,6 @@ def response_handler(client_obj):
 
 HTTP_server = fangcore.HTTPServer()
 
-HTTP_server.start_http_server("192.168.1.18", 80, 5)
+HTTP_server.start_http_server("192.168.1.18", 80, 1)
 
 HTTP_server.set_response_method(response_handler)
