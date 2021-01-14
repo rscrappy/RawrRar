30da7c4f07074f1ef0c784dab4d7e031aee6fe89
Furrure <71794171+Furrure@users.noreply.github.com>
2021-01-13 21:30:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ pages = {}
 opened = open("Rawr-main.html", "rb")
 pages['home'] = opened.read()
 opened.close()
-
+HTTP_server = None
 def response_handler(client_obj):
 	print(client_obj.split_request)
 	if client_obj.split_request == []:
 		client_obj.set_page(pages['home'])
+	elif client_oj.split_request[0] == "REINIT":
+		HTTP_server.stop_http_server()
+		quit()
 	else:
 		opened = open(client_obj.split_request[0], "rb")
 		client_obj.set_page(opened.read())
