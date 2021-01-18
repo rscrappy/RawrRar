7f967a3e13588afb3747a22f4fe94eb571f92c00
Furrure <71794171+Furrure@users.noreply.github.com>
2021-01-17 19:25:27 -0500

    Added 404 and robots.txt

--- main.py
+++ main.py
@@ -19,7 +19,10 @@ def response_handler(client_obj):
 		if client_obj.split_request[0] == "REINIT":
 			HTTP_server.stop_http_server()
 			return
-		client_obj.set_page(pages[client_obj.split_request[0]])
+		try:
+			client_obj.set_page(pages[client_obj.split_request[0]])
+		except KeyError:
+			client_obj.set_response_header(b"404 Page Not Found")
 
 HTTP_server = fangcore.HTTPServer()
 
