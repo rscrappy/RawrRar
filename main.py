2384fdab2bccab6bc97c0f71bf4fcb7a87dd2837
Furrure <71794171+Furrure@users.noreply.github.com>
2021-01-17 19:11:53 -0500

    main.py is now more efficient when serving files
    Loads all files before starting the server, rather than serving per-request

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 import fangcore
+import os
 
 pages = {}
 
-opened = open("HTML/Rawr-main.html", "rb")
-pages['home'] = opened.read()
-opened.close()
+
+for file in os.listdir("HTML"):
+	opened = open("HTML/" + file, "rb")
+	pages[file] = opened.read()
+	opened.close()
+
 HTTP_server = None
 def response_handler(client_obj):
 
 	if client_obj.split_request == []:
-		client_obj.set_page(pages['home'])
+		client_obj.set_page(pages['Rawr-main.html'])
 		client_obj.add_tag(b"Content-Type", b"text/html")
 	else:
 		if client_obj.split_request[0] == "REINIT":
 			HTTP_server.stop_http_server()
 			return
-		opened = open("HTML/" + client_obj.split_request[0], "rb")
-		client_obj.set_page(opened.read())
-		opened.close()
+		client_obj.set_page(pages[client_obj.split_request[0]])
 
 HTTP_server = fangcore.HTTPServer()
 
-HTTP_server.start_http_server("192.168.1.18", 80, 1)
+HTTP_server.start_http_server("localhost", 80, 1)
 
 HTTP_server.set_response_method(response_handler)
 
