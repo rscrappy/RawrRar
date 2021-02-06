2086127ae49548bcccd879d435cb8483e5420401
RoccoLoxPrograms <71672299+RoccoLoxPrograms@users.noreply.github.com>
2021-02-06 13:11:03 -0700

    Added "Invalid username or password"

--- HTML/JS-main.js
+++ HTML/JS-main.js
@@ -1,11 +1,11 @@
 /* January 2021 - RoccoLox Programs */
 function choiceS() {
     // if you selected student
-    localStorage.setItem("portal","Student");
+    localStorage.setItem('portal','Student');
 }
 function choiceT() {
     // if you selected teacher
-    localStorage.setItem("portal","Teacher");
+    localStorage.setItem('portal','Teacher');
 }
 function signout() {
     // asks if you want to sign out
@@ -19,7 +19,7 @@ function signout() {
     }
 }
 // gets 'Student' or 'Teacher' from localStorage
-var portal = localStorage.getItem("portal");
+var portal = localStorage.getItem('portal');
 // changes title of login page
 document.getElementById('loginTitle').innerHTML = 'RawrRar - ' + portal + ' Login';
 // changes login portal name
@@ -34,10 +34,17 @@ if (portal === 'Student') {
 }
 // tells what page to submit to
 function submit() {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    localStorage.setItem('password',document.getElementById('password').value);
     localStorage.setItem('username',document.getElementById('username').value);
-    if (portal === 'Student') {
-        window.location.href = "Rawr-overview.html";
+    if (username.length > 0 && password.length > 0) {
+        if (portal === 'Student') {
+            window.location.href = "Rawr-overview.html";
+        } else {
+            window.location.href = "Rawr-overview(teacher).html";
+        }
     } else {
-        window.location.href = "Rawr-overview(teacher).html";
+        document.getElementById('errorLogin').innerHTML = 'Invalid username or password';
     }
 }
